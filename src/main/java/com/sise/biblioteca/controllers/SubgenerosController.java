038601600056782ba5e4bf20f83d32cb0bfda97b
Eduardo Quea <eduardoquea3@gmail.com>
2024-09-14 18:00:30 -0500

    feat: add controllers with all routes required

---
+++ src/main/java/com/sise/biblioteca/controllers/SubgenerosController.java
@@ -0,0 +1,60 @@
+package com.sise.biblioteca.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sise.biblioteca.shared.BaseResponse;
+
+@RestController
+@RequestMapping("/subgeneros")
+public class SubgenerosController {
+
+  // add service
+
+  @GetMapping("")
+  public ResponseEntity<BaseResponse> getAll() {
+    try {
+      return new ResponseEntity<BaseResponse>(BaseResponse.success(), HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<BaseResponse>(BaseResponse.error(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @PostMapping("")
+  // change type class
+  public ResponseEntity<BaseResponse> add(@RequestBody Integer subgenero) {
+    try {
+      return new ResponseEntity<BaseResponse>(BaseResponse.success(), HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<BaseResponse>(BaseResponse.error(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @PutMapping("/{idSubgenero}")
+  // change type class
+  public ResponseEntity<BaseResponse> edit(@PathVariable Integer idSubgenero, @RequestBody Integer subgenero) {
+    try {
+      return new ResponseEntity<BaseResponse>(BaseResponse.success(), HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<BaseResponse>(BaseResponse.error(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @PatchMapping("/{idSubgenero}")
+  public ResponseEntity<BaseResponse> remove(@PathVariable Integer idSubgenero) {
+    try {
+      return new ResponseEntity<BaseResponse>(BaseResponse.success(), HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<BaseResponse>(BaseResponse.error(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+}

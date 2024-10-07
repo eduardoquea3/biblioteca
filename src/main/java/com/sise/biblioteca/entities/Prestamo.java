37d737291a4c44726c589f4cc21b354b5d4575b7
soy Nelson <brazymoreno11@gmail.com>
2024-10-06 23:13:17 -0500

    feat:add new entities

---
+++ src/main/java/com/sise/biblioteca/entities/Prestamo.java
@@ -0,0 +1,47 @@
+package com.sise.biblioteca.entities;
+
+import java.sql.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "prestamos")
+public class Prestamo {
+      
+      @Id
+      @Column(name = "idprestamo",insertable = false,updatable = false)
+      @GeneratedValue(strategy =GenerationType.IDENTITY )
+      private Integer idPrestamo;
+
+      @OneToOne
+      @JoinColumn(name = "idcliente",nullable = false)
+      private Cliente cliente;
+      
+
+      @Column(name="cantidad")
+      private Integer cantidad;
+      
+      @Column(name="fechaprestamo")
+      private Date fechaPrestamo;
+
+      @Column(name="fechadevolucion")
+      private Date fechaDevolucion;
+     
+      @Column(name = "estadoauditoria",insertable = false,updatable = false)
+      @JsonIgnore
+      private boolean estado;
+
+}

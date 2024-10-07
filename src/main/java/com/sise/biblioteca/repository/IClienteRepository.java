61aeafc212b7769847ee8241a8103a34a7e4b056
soy Nelson <brazymoreno11@gmail.com>
2024-10-06 23:13:35 -0500

    feat:add new repository

---
+++ src/main/java/com/sise/biblioteca/repository/IClienteRepository.java
@@ -0,0 +1,25 @@
+package com.sise.biblioteca.repository;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.sise.biblioteca.entities.Cliente;
+
+import jakarta.transaction.Transactional;
+
+public interface IClienteRepository extends JpaRepository <Cliente ,Integer> {
+
+   Page<Cliente> findByEstado(boolean estado,Pageable pageable);
+
+   Cliente findOneByIdClienteAndEstado(Integer idCliente,boolean estado);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Cliente a SET a.estado = false WHERE a.idCliente =:idCliente ")
+     void remove(@Param("idCliente") Integer idCliente);
+      
+}

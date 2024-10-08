45864873c29ff9ecfeecee67d44609e84f7cac6f
Eduardo Quea <eduardoquea3@gmail.com>
2024-10-08 15:21:26 -0500

    styles: format files

--- src/main/java/com/sise/biblioteca/service/IDevolucionService.java
+++ src/main/java/com/sise/biblioteca/service/IDevolucionService.java
@@ -1,21 +1,19 @@
 package com.sise.biblioteca.service;
-import org.springframework.data.domain.Pageable;
 
 import com.sise.biblioteca.entities.Devolucion;
 import com.sise.biblioteca.errors.ClientException;
-
-
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface IDevolucionService {
-      
-      Page<Devolucion> getAll(Pageable pageable);
 
-      Devolucion getById(Integer idDevolucion) throws ClientException;
+  Page<Devolucion> getAll(Pageable pageable);
 
-      Devolucion add(Devolucion devolucion);
+  Devolucion getById(Integer idDevolucion) throws ClientException;
 
-      Devolucion edit(Integer id, Devolucion devolucion) throws ClientException;
+  Devolucion add(Devolucion devolucion);
 
-      void remove(Integer idDevolucion) throws ClientException;
+  Devolucion edit(Integer id, Devolucion devolucion) throws ClientException;
 
+  void remove(Integer idDevolucion) throws ClientException;
 }
--- src/main/java/com/sise/biblioteca/service/IEstadoPrestamoService.java
+++ src/main/java/com/sise/biblioteca/service/IEstadoPrestamoService.java
@@ -1,22 +1,19 @@
 package com.sise.biblioteca.service;
 
-
-import org.springframework.data.domain.Pageable;
-
 import com.sise.biblioteca.entities.EstadoPrestamo;
 import com.sise.biblioteca.errors.ClientException;
-
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface IEstadoPrestamoService {
-      
-      Page<EstadoPrestamo>getAll(Pageable pageable);
 
-      EstadoPrestamo getById(Integer idEstadoPrestamo) throws ClientException;
+  Page<EstadoPrestamo> getAll(Pageable pageable);
 
-      EstadoPrestamo add(EstadoPrestamo estadoPrestamo);
+  EstadoPrestamo getById(Integer idEstadoPrestamo) throws ClientException;
 
-      EstadoPrestamo edit(Integer id,EstadoPrestamo estadoPrestamo) throws ClientException;
+  EstadoPrestamo add(EstadoPrestamo estadoPrestamo);
 
-      void remove(Integer idEstadoPrestamo) throws ClientException;
+  EstadoPrestamo edit(Integer id, EstadoPrestamo estadoPrestamo) throws ClientException;
+
+  void remove(Integer idEstadoPrestamo) throws ClientException;
 }
